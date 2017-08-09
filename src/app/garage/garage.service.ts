fc831e724957b94e3b0fcf65e5f3b397c65541ff
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-09 18:47:26 -0300

    Remove authentication from garage service

--- src/app/garage/garage.service.ts
+++ src/app/garage/garage.service.ts
@@ -1,17 +1,15 @@
 import {Injectable} from "@angular/core";
-import {Garage} from "./garage";
-import {Headers, Http, RequestOptions} from "@angular/http";
+import {Http} from "@angular/http";
 import {Router} from "@angular/router";
 
 @Injectable()
 export class GarageService {
   private garageUrl = 'http://localhost:4000/api/garage';
 
-  constructor(
-    private http: Http,
-    private router : Router) {
+  constructor(private http: Http,
+              private router: Router) {
 
   }
 
-  }
+}
 
