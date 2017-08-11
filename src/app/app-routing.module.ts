0a73b0ade2dccd6ced07a259474fd8df8dcc34e5
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-11 18:31:43 -0300

    Added logout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,26 @@ import {LoginComponent} from './login/login.component';
 import {SignUpComponent} from './signup/signup.component';
 import {AuthGuard} from './guards/auth-guard.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {HomeComponent} from './home/home.component';
+import {LogoutComponent} from './logout/logout.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     component: LoginComponent
+  },
+  { path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'signup',
