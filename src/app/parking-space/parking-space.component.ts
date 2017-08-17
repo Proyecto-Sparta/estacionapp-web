52e55396ef693db5a465985447b0c6d90ebf2f7a
Bianca Petracca <bianca.petracca@despegar.com>
2017-08-17 15:11:59 -0300

    Added size and position to parking spaces

--- src/app/parking-space/parking-space.component.ts
+++ src/app/parking-space/parking-space.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, HostListener} from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 import * as interact from 'interactjs';
 
 @Component({
@@ -8,23 +8,35 @@ import * as interact from 'interactjs';
 })
 export class ParkingSpaceComponent implements AfterViewInit {
 
-  constructor() {
+  private angle = 0;
+  private x = 0;
+  private y = 0;
+  private width;
+  private height;
+  private elementHTML : HTMLElement;
+
+  constructor(private elemRef: ElementRef) {
   }
 
   ngAfterViewInit(): void {
-    this.setupDraggable();
+    this.setupDraggable(this);
+    this.elementHTML = this.elemRef.nativeElement;
+    this.height = this.elementHTML.getBoundingClientRect().height;
+    this.width = this.elementHTML.getBoundingClientRect().width;
   }
 
-  private setupDraggable() {
+
+  private setupDraggable(component) {
     interact('.parking-space').draggable({
       inertia: true,
+      endOnly: false,
       restrict: {
         restriction: '.dropzone',
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        elementRect: {top: 0, left: 0, bottom: 1, right: 1}
       },
 
       autoScroll: true,
-      onmove: function (event) {
+      onmove: function(event){
         let target = event.target,
 
           x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -36,31 +48,14 @@ export class ParkingSpaceComponent implements AfterViewInit {
 
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
+
+        component.x = -x;
+        component.y = -y;
+        component.width = component.elementHTML.getBoundingClientRect().width;
+        component.height = component.elementHTML.getBoundingClientRect().height;
+
       }
     })
-      .resizable({
-        preserveAspectRatio: true,
-        edges: { left: false, right: true, bottom: true, top: false },
-      })
-      .on('resizemove', function (event) {
-        let target = event.target,
-          x = (parseFloat(target.getAttribute('data-x')) || 0),
-          y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-
-        target.style.width  = event.rect.width + 'px';
-        target.style.height = event.rect.height + 'px';
-
-        x += event.deltaRect.left;
-        y += event.deltaRect.top;
-
-        target.style.webkitTransform = target.style.transform =
-          'translate(' + x + 'px,' + y + 'px)';
-
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      });
-
   }
 
 }
