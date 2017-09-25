76bcb0c808f6e477c633c46a0e3eec76d239b49e
sebastianBrunoUtn <sebastianbruno.utn@gmail.com>
2017-09-25 10:46:32 -0300

    Periodicamente chequea si hay notificaciones nuevas

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 import {Notification} from '../notifications/notification';
 
 @Injectable()
 export class NotificationsService {
   private notificationsUrl = 'http://localhost:4200';
+  private refreshPeriod = 3000;
 
   constructor(private http: Http) {
   }
 
   public getNotificationsObservable(garageId: Number) {
-    return this.http.get(this.notificationsUrl)
-      .map(data => new Notification(Notification.driverOnTheWay, 123));
+    return Observable
+      .interval(this.refreshPeriod)
+      .switchMap(_ => this.http.get(this.notificationsUrl))
+      .map(data => new Notification(Notification.driverOnTheWay, 123)); // (Mocked)
   }
 }
