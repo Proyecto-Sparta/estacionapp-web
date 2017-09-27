989d8cbc73a43b342aedf4922f66ad61e640489c
sebastianBrunoUtn <sebastianbruno.utn@gmail.com>
2017-09-27 11:10:43 -0300

    Muestra las notificaciones

--- src/app/myAccount/myAccount.component.ts
+++ src/app/myAccount/myAccount.component.ts
@@ -15,12 +15,25 @@ import {Observable} from 'rxjs/Rx';
 export class MyAccountComponent {
 
   private notificationsObs: Observable<Notification>;
+  private notifications: Array<String> = [];
 
   constructor(private loginService: LoginService,
               private notificationsService: NotificationsService,
               private router: Router) {
     this.notificationsObs = notificationsService.getNotificationsObservable(666);
-    this.notificationsObs.subscribe(data => console.log(data));
+    this.notificationsObs.subscribe(notification => {
+      const notificationText = this.processNotification(notification);
+      this.notifications.push(notificationText);
+    });
+  }
+
+  private processNotification(notification) {
+    switch (notification.type) {
+      case Notification.driverOnTheWay:
+        return 'New driver on the way!';
+      default:
+        return 'Ni idea';
+    }
   }
 
 }
