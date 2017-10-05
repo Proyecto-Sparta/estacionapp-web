c3b6554759f5c73e6f78fa8ae5a5375d9aef0709
Bianca Petracca <bianca.petracca@despegar.com>
2017-10-05 01:45:50 -0300

    Improved notification views

---
+++ src/app/notification-stream/notification-stream.ts
@@ -0,0 +1,21 @@
+import {Socket, Channel} from 'phoenix';
+
+export class NotificationStream {
+
+  channel: Channel;
+  id: String;
+
+  constructor(id : String, callback : any){
+    let socket = new Socket("ws://localhost:4000/socket", {});
+    socket.connect();
+
+    this.channel = socket.channel(`garage:${id}`, {});
+
+    this.channel.on("request", callback);
+
+    this.channel.join()
+      .receive("ok", resp => { console.log("Garage joined successfully") })
+      .receive("error", resp => { console.log("Unable to join", resp) });
+  }
+
+}
