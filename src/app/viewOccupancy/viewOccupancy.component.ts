9a8f5b87c483b601b1f4fc8858d40bbeef9ef870
sebastianBrunoUtn <sebastianbruno.utn@gmail.com>
2017-10-13 10:55:20 -0300

    Implementacion visual de asignacion de espacios a conductores

--- src/app/viewOccupancy/viewOccupancy.component.ts
+++ src/app/viewOccupancy/viewOccupancy.component.ts
@@ -16,6 +16,10 @@ export class ViewOccupancyComponent implements AfterViewInit {
   private floors;
   private layoutScale;
   private currentFloor = 0;
+  private selectedDriverIndex = -1;
+  
+  //Mock, obtener de service
+  private drivers = ['Driver1', 'Driver2', 'Driver3'];
 
   @ViewChild('garage') garage: ElementRef;
   @ViewChildren(ParkingSpaceComponent) viewChildren;
@@ -33,11 +37,6 @@ export class ViewOccupancyComponent implements AfterViewInit {
         .getFloorPlans(666)
         .then((floors) => this.applyScale(floors, this.layoutScale))
         .then((storedFloors) => this.floors = storedFloors);
-  }
-
-  toggleOccupancy(parkingSpaceIndex: number) {
-    this.floors[this.currentFloor].parkingSpaces[parkingSpaceIndex].toggleOccupancy();
-    // Update on DB
   }
 
   private applyScale(floors, scale) {
@@ -59,5 +58,29 @@ export class ViewOccupancyComponent implements AfterViewInit {
 
     this.viewChildren.forEach(child => child.updatePosition(child));
     this.currentFloor += 1;
+  }
+
+  private selectDriver(driverIndex) {
+    this.selectedDriverIndex = driverIndex;
+  }
+
+
+  private toggleOccupancy(parkingSpaceIndex: number) {
+    const parkingSpace = this.floors[this.currentFloor].parkingSpaces[parkingSpaceIndex],
+      isOccupied = parkingSpace.occupied,
+      driverSelected = this.selectedDriverIndex !== -1;
+
+    if (!isOccupied && !driverSelected) {
+      console.log("Select a driver first!");
+      return;
+    }
+
+    if (!isOccupied && driverSelected) {
+      // TODO: Assign space to driver
+      this.drivers = this.drivers.filter((_, i) => i !== this.selectedDriverIndex);
+      this.selectedDriverIndex = -1;
+    }
+
+    parkingSpace.toggleOccupancy();
   }
 }
