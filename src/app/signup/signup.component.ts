d4608978d830779ba4ac8e0934da13b5f34e956c
Bianca Petracca <bianca.petracca@despegar.com>
2017-11-22 11:57:53 -0300

    Added signup request

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {mapListener} from '../interfaces/mapListener';
 import {GarageService} from '../garage/garage.service';
 import {Marker} from '../map/map.component';
 import {SignUpModelValidator} from './signupModelValidator';
 import {AmenityCheckbox, SignupModel} from "./signup";
 import {SignupService} from "./signup.service";
+import {Router} from "@angular/router";
+import {AlertComponent} from "app/alert/alert.component";
 
 @Component({
   selector: 'signup',
@@ -14,13 +16,18 @@ import {SignupService} from "./signup.service";
 })
 export class SignUpComponent implements mapListener, OnInit {
 
+  @ViewChild(AlertComponent) alertComponent;
+
   ngOnInit(): void {
     this.model.amenities = this.garageService.getAmenities()
       .map(amenity => new AmenityCheckbox(amenity, false));
   }
 
-  constructor(private garageService: GarageService, private  signupService : SignupService) {
+  constructor(private garageService: GarageService,
+              private  signupService : SignupService,
+              private router : Router) {
   }
+
 
   submitted = false;
   errors = [];
@@ -38,12 +45,10 @@ export class SignUpComponent implements mapListener, OnInit {
     const validation = this.validator.validate(this.model);
     this.errors = validation.errors;
     if(!validation.result){
-      this.signupService.signup(this.model);
+      this.signupService.signup(this.model)
+        .then(() => this.router.navigate(['/myAccount/dashboard']))
+        .catch(this.alertComponent.newError("Couldn't sign up"));
     }
-  }
-
-  getChecked() {
-    return this.model.amenities.filter(amenity => amenity.checked);
   }
 
 }
--- src/app/signup/signup.service.ts
+++ src/app/signup/signup.service.ts
@@ -1,9 +1,28 @@
 import {Injectable} from "@angular/core";
 import {SignupModel} from "./signup";
+import {Garage} from "../garage/garage";
+import {LoginService} from "../login/login.service";
+import {Http} from "@angular/http";
 
 @Injectable()
 export class SignupService {
 
-  public signup(model : SignupModel){}
+  private garageUrl = 'http://localhost:4000/api/garages';
+
+  constructor(private loginService : LoginService, private http : Http){
+
+  }
+
+  public signup(model : SignupModel){
+    const url = `${this.garageUrl}`;
+    return this.http
+      .post(url, model.mapToRequest())
+      .toPromise()
+      .then( (response) => this.loginService.login(new Garage(model.username, model.password)));
+  }
+
+
+
+
 
 }
