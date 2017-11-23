820fbae50da9b99014ddadbe92626ef2762e8c55
Bianca Petracca <bianca.petracca@despegar.com>
2017-11-23 00:50:08 -0300

    Added ids to requests

--- src/app/garage/pricing.ts
+++ src/app/garage/pricing.ts
@@ -1,17 +1,27 @@
+import {isNull} from "util";
+
 export class Pricing {
 
   constructor(public bike : number = 0,
               public car: number = 0,
-              public pickup: number = 0){
+              public pickup: number = 0,
+              public id : string = null){
 
   }
 
   public mapToRequest(){
-    return {
+    let request = {
+      id : this.id,
       bike : this.bike,
       car : this.car,
       pickup : this.pickup
-    }
+    };
+
+    if(isNull(this.id))
+      delete request.id;
+
+    return request;
+
   }
 
 }
--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -3,10 +3,24 @@ import {Pricing} from "../garage/pricing";
 
 export class SettingsModel {
 
-  constructor(public username : string,
-              public name : string,
+  constructor(public name : string,
               public email : string,
               public pricing : Pricing,
               public amenities : Array<AmenityCheckbox>){}
 
+
+
+  public mapToRequest(){
+    return {
+      id : JSON.parse(localStorage.getItem('garage'))['id'],
+      name : this.name,
+      email : this.email,
+      pricing : this.pricing.mapToRequest(),
+      amenities : this.amenities
+        .filter(amenity => amenity.checked)
+        .map(amenity => amenity.amenity.id)
+    }
+  }
 }
+
+
